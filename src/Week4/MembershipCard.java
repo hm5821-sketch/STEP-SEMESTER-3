f8a5c22533da33bee6015b041e4d69d1d5da9466
hm5821-sketch <hm5821@srmist.edu.in>
2026-09-06 12:09:47 +0530

    Added Week 4 HW Assignment

---
+++ src/Week4/MembershipCard.java
@@ -0,0 +1,29 @@
+package Week4;
+
+public class MembershipCard {
+    static String libraryName;
+    static String validUntil;
+    String studentName;
+
+    // Static block executing once when class is loaded
+    static {
+        libraryName = "SRM Central Library";
+        validUntil = "May 2027";
+        System.out.println("Library info loaded");
+    }
+
+    public MembershipCard(String studentName) {
+        this.studentName = studentName;
+    }
+
+
+
+    public static void main(String[] args) {
+        String[] names = {"Ananya", "Rohan", "Priya", "Arjun", "Sneha"};
+
+        for (int i = 0; i < names.length; i++) {
+            MembershipCard card = new MembershipCard(names[i]);
+            System.out.println("Membership card issued: " + card.studentName);
+        }
+    }
+}
